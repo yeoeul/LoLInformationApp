2bacfe1b85a04713c6af8d101c870493a8bba397
yeoeul <yeoeul.ruhe@gmail.com>
2021-01-21 11:20:34 +0900

    done_weekButton setText

--- app/src/main/java/org/techtown/lolinformationapp/MainActivity.java
+++ app/src/main/java/org/techtown/lolinformationapp/MainActivity.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.DialogInterface;
-import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.jsoup.Jsoup;
@@ -24,6 +27,9 @@ import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    LinearLayout leagueScheduleLayout;
+    LinearLayout leagueRankingLayout;
+    ArrayList<Button> weekButton = new ArrayList<Button>();
     ArrayList<TextView> rank = new ArrayList<TextView>();
     ArrayList<ImageView> teamLogo = new ArrayList<ImageView>();
     ArrayList<TextView> teamName = new ArrayList<TextView>();
@@ -38,27 +44,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+/*
         if (NetworkConnection() == false)
             NotConnected_showAlert();
-
+*/
         /*getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);*///풀스크린
 
+
         initView();
+
+        leagueRankingLayout = this.findViewById(R.id.leagueRankingLayout);
+        leagueScheduleLayout = this.findViewById(R.id.leagueScheduleLayout);
+        //leagueScheduleLayout.setVisibility(View.GONE);
+        leagueRankingLayout.setVisibility(View.GONE);
+
+        new Thread(new Runnable(){
+            public void run(){
+                Elements elements = bringScheduleData();
+                String str[] = elements.text().split(" ");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        for(int i = 0; i < str.length; i += 2) weekButton.get(i / 2).setText(str[i] + " " + str[i + 1]);
+                    }
+                });
+            }
+        }).start();
 
         new Thread() {
             public void run(){
                 bringRankingData();
-                Message msg = handler.obtainMessage();
-                handler.sendMessage(msg);
+                Message msg = teamLogoHandler.obtainMessage();
+                teamLogoHandler.sendMessage(msg);
             }
         }.start();
-
-        setTeamLogo();
     }
 
     private void initView() {
+        for(int i = 1; i <= 10; i++){
+            String array = "week" + i + "Button";
+            int resID = getResources().getIdentifier(array, "id", getPackageName());
+            weekButton.add((findViewById(resID)));
+        }
         for(int i = 0; i < 6; i++){
             for(int j = 0; j < 10; j++){
                 String array = dataList.get(i) + (j + 1);
@@ -71,7 +99,17 @@ public class MainActivity extends AppCompatActivity {
             int resID = getResources().getIdentifier(array, "id", getPackageName());
             teamLogo.add((findViewById(resID)));
         }
+    }
 
+    private Elements bringScheduleData() {
+        Elements elements = null;
+        try{
+            Document doc = Jsoup.connect("https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&mra=bjFE&pkid=475&os=17568053&query=2021%20LoL%20%EC%B1%94%ED%94%BC%EC%96%B8%EC%8A%A4%20%EC%BD%94%EB%A6%AC%EC%95%84%20%EC%8A%A4%ED%94%84%EB%A7%81%20%EB%A6%AC%EA%B7%B8%EC%9D%BC%EC%A0%95").get();
+            elements = doc.select(".this_text");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return elements;
     }
 
     private void bringRankingData() {
@@ -90,31 +128,28 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
-    final Handler handler = new Handler() {
+
+    final Handler teamLogoHandler = new Handler() {
         public void handleMessage(Message msg) {
             setTeamLogo();
         }
     };
 
     private void setTeamLogo(){
-        try{
-            String text = "";
-            for(int i = 0; i < 10; i++) {
-                text = dataGroup.get(1).get(i).getText().toString();
+        String text = "";
+        for(int i = 0; i < 10; i++) {
+            text = dataGroup.get(1).get(i).getText().toString();
 
-                if(text.equals("DRX")) teamLogo.get(i).setImageResource(R.drawable.drx);
-                else if(text.equals("KT")) teamLogo.get(i).setImageResource(R.drawable.kt);
-                else if(text.equals("T1")) teamLogo.get(i).setImageResource(R.drawable.t1);
-                else if(text.equals("농심")) teamLogo.get(i).setImageResource(R.drawable.nongsim);
-                else if(text.equals("담원 기아")) teamLogo.get(i).setImageResource(R.drawable.damwon);
-                else if(text.equals("리브 샌박")) teamLogo.get(i).setImageResource(R.drawable.sandbox);
-                else if(text.equals("아프리카")) teamLogo.get(i).setImageResource(R.drawable.afreeca);
-                else if(text.equals("젠지")) teamLogo.get(i).setImageResource(R.drawable.geng);
-                else if(text.equals("프레딧")) teamLogo.get(i).setImageResource(R.drawable.fredit);
-                else if(text.equals("한화생명")) teamLogo.get(i).setImageResource(R.drawable.hanhwa);
-            }
-        } catch (Exception e){
-            e.printStackTrace();
+            if(text.equals("DRX")) teamLogo.get(i).setImageResource(R.drawable.drx);
+            else if(text.equals("KT")) teamLogo.get(i).setImageResource(R.drawable.kt);
+            else if(text.equals("T1")) teamLogo.get(i).setImageResource(R.drawable.t1);
+            else if(text.equals("농심")) teamLogo.get(i).setImageResource(R.drawable.nongsim);
+            else if(text.equals("담원 기아")) teamLogo.get(i).setImageResource(R.drawable.damwon);
+            else if(text.equals("리브 샌박")) teamLogo.get(i).setImageResource(R.drawable.sandbox);
+            else if(text.equals("아프리카")) teamLogo.get(i).setImageResource(R.drawable.afreeca);
+            else if(text.equals("젠지")) teamLogo.get(i).setImageResource(R.drawable.geng);
+            else if(text.equals("프레딧")) teamLogo.get(i).setImageResource(R.drawable.fredit);
+            else if(text.equals("한화생명")) teamLogo.get(i).setImageResource(R.drawable.hanhwa);
         }
     }
 
